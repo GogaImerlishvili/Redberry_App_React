adbcdfebfb894e64d12b6965e1c4fa199a9d197c
Goga <gogaimerlishvili1@gmail.com>
2022-07-30 03:29:50 +0400

    added page about covid stuff survey && styles

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useContext } from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import React, { useState, useContext } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { AuthContext } from "../providers/AuthProvider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import PreferToWork from "../atoms/Page2/PreferToWork";
@@ -7,9 +7,10 @@ import HasCovid from "../atoms/Page2/HasCovid";
 import When from "../atoms/Page2/When";
 import Vaccinated from "../atoms/Page2/Vaccinated";
 import WhenVaccinated from "../atoms/Page2/WhenVaccinated";
+import classes from "./Page2.module.css";
 
 const Page2 = () => {
-  const { setUser, data, setCovid } = useContext(AuthContext);
+  const { data, setCovid } = useContext(AuthContext);
   const [vaccinated, setVaccinated] = useState(data.vaccinated);
   const [hasCovid, setHasCovid] = useState(data.had_covid);
   const [preffer, setPreffer] = useState("");
@@ -47,57 +48,65 @@ const Page2 = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="left">
-          <h2 className="title_page2">Covid Stuff</h2>
-        </div>
-        <form className="form-wrapper" onSubmit={onSubmit}>
-          <div>
-            <PreferToWork name="prefer" setPreffer={setPreffer} />
-
-            <HasCovid
-              name="covid"
-              val={data.had_covid}
-              value={hasCovid}
-              setHasCovid={setHasCovid}
-            />
-
-            {hasCovid && <When name="date" val={data.had_covid_at} />}
-
-            <Vaccinated
-              name="vaccinated"
-              value={vaccinated}
-              setVaccinated={setVaccinated}
-            />
-
-            {vaccinated && (
-              <WhenVaccinated name="dateLast" val={data.vaccinated_at} />
-            )}
-            <div className="container-dots">
-              {/* here should be array */}
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-            </div>
+      <div className={classes["main_container"]}>
+        <div className={classes.left}>
+          <div className={classes.title}>
+            <h1>Covid Stuff</h1>
           </div>
+          <form className={classes["form_wrapper"]} onSubmit={onSubmit}>
+            <div>
+              <div className={classes.name}>
+                <PreferToWork name="prefer" setPreffer={setPreffer} />
 
-          <button
-            className="left-arrow"
-            onClick={() => {
-              backPage();
-            }}
-          >
-            <FaChevronLeft />
-          </button>
-          <button type="submit" className="right-arrow">
-            <FaChevronRight />
-          </button>
-        </form>
-        <div className="right">
-          <h2 className="other_title">Redberry Covid Policies</h2>
-          <p className="content_covid">
+                <HasCovid
+                  name="covid"
+                  val={data.had_covid}
+                  value={hasCovid}
+                  setHasCovid={setHasCovid}
+                />
+
+                {hasCovid && <When name="date" val={data.had_covid_at} />}
+
+                <Vaccinated
+                  name="vaccinated"
+                  value={vaccinated}
+                  setVaccinated={setVaccinated}
+                />
+
+                {vaccinated && (
+                  <WhenVaccinated name="dateLast" val={data.vaccinated_at} />
+                )}
+                <div className={classes["container-dots"]}>
+                  {/* here should be array */}
+                  <button
+                    type="button"
+                    className={classes["left-arrow"]}
+                    onClick={backPage}
+                  >
+                    <AiOutlineArrowLeft />
+                  </button>
+                  <div className={classes.dot}></div>
+                  <div className={classes.dot}></div>
+                  <div className={classes.dot}></div>
+                  <div className={classes.dot}></div>
+                  <div className={classes.dot}></div>
+                  <button
+                    type="submit"
+                    onClick={nextPage}
+                    className={classes["right-arrow"]}
+                  >
+                    <AiOutlineArrowRight />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className={classes.right}>
+          <div className={classes["right-title"]}>
+            <h1>Redberry Covid Policies</h1>
+          </div>
+          <p className={classes.content}>
             As this infamous pandemic took over the world, we adjusted our
             working practices so that our employees can be as safe and
             comfortable as possible. We have a hybrid work environment, so you
