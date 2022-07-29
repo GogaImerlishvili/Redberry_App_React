b3dd41b6aaf96e24b1c8c36152fd8fc2e7bd78d1
Goga <gogaimerlishvili1@gmail.com>
2022-07-30 02:37:39 +0400

    added main context && form && styles

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -4,6 +4,8 @@ import { SkillsInput } from "../atoms/Page1/SkillsInput/SkillsInput";
 // import { ExperienceInput } from "../atoms/Page1/SkillsInput/ExperienceInput";
 import { useNavigate } from "react-router-dom";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import classes from "./Page1.module.css";
 const Page1 = () => {
   const { setUser, data, addSkill } = useContext(AuthContext);
 
@@ -39,45 +41,46 @@ const Page1 = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="left">
-          <h2 className="title">Tell us about your skills</h2>
-        </div>
-        <form className="form-wrapper" onSubmit={onSubmit}>
-          <div>
-            <div className="name">
-              <SkillsInput name="name" val={data.skills} add={x} />
-            </div>
-            <div className="container-dots">
-              {/* here should be array */}
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-              <div className="dot"></div>
-            </div>
+      <div className={classes["main_container"]}>
+        <div className={classes.left}>
+          <div className={classes.title}>
+            <h1>Tell us about your skills</h1>
           </div>
-          <button
-            className="left-arrow"
-            onClick={() => {
-              backPage();
-            }}
-          >
-            <FaChevronLeft />
-          </button>
-          <button
-            className="right-arrow"
-            type="submit"
-            onClick={() => {
-              nextPage();
-            }}
-          >
-            <FaChevronRight />
-          </button>
-        </form>
-        <div className="right">
-          <h2 className="other_title page1_title">Redberry Origins</h2>
-          <p className="content">
+          <form className={classes["form_wrapper"]} onSubmit={onSubmit}>
+            <div>
+              <div className={classes.name}>
+                <SkillsInput name="name" val={data.skills} add={x} />
+              </div>
+              <div className={classes["container-dots"]}>
+                {/* here should be array */}
+                <button
+                  type="button"
+                  className={classes["left-arrow"]}
+                  onClick={backPage}
+                >
+                  <AiOutlineArrowLeft />
+                </button>
+                <div className={classes.dot}></div>
+                <div className={classes.dot}></div>
+                <div className={classes.dot}></div>
+                <div className={classes.dot}></div>
+                <div className={classes.dot}></div>
+                <button
+                  type="submit"
+                  onClick={nextPage}
+                  className={classes["right-arrow"]}
+                >
+                  <AiOutlineArrowRight />
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className={classes.right}>
+          <div className={classes["right-title"]}>
+            <h1>Redberry Origins</h1>
+          </div>
+          <p className={classes.content}>
             You watch “What? Where? When?” Yeah. Our founders used to play it.
             That’s where they got a question about a famous American author and
             screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the
