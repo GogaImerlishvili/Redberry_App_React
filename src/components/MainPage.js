54cc9a6cb9f9874acd4d13deb070f20fa7646413
Goga <gogaimerlishvili1@gmail.com>
2022-07-30 05:17:37 +0400

    added submitted && start buttons && styles

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -40,13 +40,17 @@ const MainPage = () => {
     <div className={classes["main_container"]}>
       <div className={classes.title}>
         <h1>welcome rocketeer!</h1>
-        <div className={classes["start-button"]}>
-          <button type="button" onClick={nextPage}>
-            Start Questionnaire
-          </button>
-        </div>
-        <div className={classes["submitted-button"]}>
-          <button onClick={submitted}>Submitted Button</button>
+        <div className={classes.content}>
+          <div className={classes["start-button"]}>
+            <button type="button" onClick={nextPage}>
+              Start Questionnaire
+            </button>
+            <div className={classes["submitted-button"]}>
+              <button type="button" onClick={submitted}>
+                Submitted Button
+              </button>
+            </div>
+          </div>
         </div>
         <div className={classes.img}>
           <img src={RocketMan} alt="rocket man" />
